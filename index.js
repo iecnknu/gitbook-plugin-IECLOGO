79f7ab4409ba97cfbce3a8333fb5a7370ca7e718
dreamdomroy <dreamdomroy@gmail.com>
2018-12-23 13:58:53 +0800

    favicon add

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ module.exports = {
             }
             // page.content = page.content.replace('https://www.gitbook.com','http://www.joinforwin.com:90').replace('Published with GitBook','Published by Joinforwin')
             return page
+        },
+        "finish" : function () {
+            var pathFile = this.options.pluginsConfig && this.options.pluginsConfig.favicon;
+            var favicon = path.join(process.cwd(), pathFile);
+            var gitbookFaviconPath = path.join(process.cwd(), '_book', 'gitbook', 'images', 'favicon.ico');
+            if (pathFile && fs.existsSync(pathFile) && fs.existsSync(gitbookFaviconPath)){
+                fs.unlinkSync(gitbookFaviconPath);
+                fs.createReadStream(favicon).pipe(fs.createWriteStream(gitbookFaviconPath));
+            }
         }
     }
 }
