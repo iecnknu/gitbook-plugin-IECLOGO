1742865dc052221b377477797570a7dc8d19a065
dreamdomroy <dreamdomroy@gmail.com>
2018-12-23 14:24:37 +0800

    select icon by javascript

--- assets/rmlink.js
+++ assets/rmlink.js
@@ -6,6 +6,9 @@ require([
         var link = $('.gitbook-link')
         link.attr('href', 'https://www.nknucsec.com')
         link.text('Published with IEC, NKNU')
+
+        var icon_link = $('link[rel=\'shortcut icon\']')
+        icon_link.attr('href', 'https://drive.google.com/uc?export=download&id=1hvAlY4gOjjzHm4XA-Wv64A1_dX9z0pk0')
     }
     changeLink()
     gitbook.events.on('page.change', function () {
