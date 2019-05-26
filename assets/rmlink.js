2920a4f3522e0a930c578e42ef56830921a6c4cc
iecnknu <43192635+iecnknu@users.noreply.github.com>
2019-05-27 00:28:16 +0800

    Update rmlink.js

--- assets/rmlink.js
+++ assets/rmlink.js
@@ -5,8 +5,8 @@ require([
     function changeLink() {
         var link = $('.gitbook-link');
         link.attr('href', 'https://www.nknucsec.com');
-        link.text('Published with IEC, NKNU');
-        link.before('<div class="gitbook-link" style="text-align: center;">' + link.text() + '<br><div style="letter-spacing: 3px;">資訊教育中心授權使用<br>著作權所有，請勿轉載</div></div>');
+        link.text('Authorized by IEC, NKNU');
+        link.before('<div class="gitbook-link" style="text-align: center;"><strong>' + link.text() + '</strong><br><div style="letter-spacing: 5px;">國立高雄師範大學</div><br><div style="letter-spacing: 4px;">資訊教育中心授權使用<br>著作權所有，請勿轉載</div></div>');
         link.remove();
         var icon_link = $('link[rel=\'shortcut icon\']');
         icon_link.attr('href', 'https://drive.google.com/uc?export=download&id=1hvAlY4gOjjzHm4XA-Wv64A1_dX9z0pk0');
